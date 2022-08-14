f4c8a5e10b953f9328c5b829be7113cfd7d5221e
Thad Ryker <thad@edgerunner.org>
2022-08-14 18:02:28 -0400

    Added support for backgroundcolor and fontstyle to the syntax guide.

--- Org.Edgerunner.Moo.Editor/SyntaxHighlighting/MooSyntaxHighlightingGuide.cs
+++ Org.Edgerunner.Moo.Editor/SyntaxHighlighting/MooSyntaxHighlightingGuide.cs
@@ -153,12 +153,270 @@ public class MooSyntaxHighlightingGuide : ISyntaxHighlightingGuide
 
       public Color GetTokenBackgroundColor(DetailedToken token, DetailedToken previousToken, DetailedToken nextToken)
       {
-         return Color.Transparent;
+         var tokenTypeName = token.TypeNameUpperCase;
+         switch (tokenTypeName)
+         {
+            case "WHILE":
+            case "ENDWHILE":
+            case "FOR":
+            case "ENDFOR":
+            case "BREAK":
+            case "CONTINUE":
+            case "RETURN":
+            case "FORK":
+            case "ENDFORK":
+            case "TRY":
+            case "EXCEPT":
+            case "FINALLY":
+            case "ENDTRY":
+            case "IF":
+            case "ELSE":
+            case "ELSEIF":
+            case "ENDIF":
+            case "ANY":
+               return Settings.Instance.KeywordBackgroundColor;
+            case "OBJECT":
+               return Settings.Instance.ObjectBackgroundColor;
+            case "CORE_REFERENCE":
+               return Settings.Instance.CoreReferenceBackgroundColor;
+            case "STRING":
+               return Settings.Instance.StringBackgroundColor;
+            case "NUMBER":
+            case "FLOAT":
+            case "ERROR":
+            case "BOOLEAN":
+               return Settings.Instance.LiteralBackgroundColor;
+            case "IDENTIFIER":
+               switch (token.DisplayText)
+               {
+                  case "player":
+                  case "caller":
+                  case "this":
+                  case "verb":
+                  case "args":
+                  case "argstr":
+                  case "dobj":
+                  case "dobjstr":
+                  case "prepstr":
+                  case "iobj":
+                  case "iobjstr":
+                  case "ERR":
+                  case "STR":
+                  case "BOOL":
+                  case "INT":
+                  case "NUM":
+                  case "FLOAT":
+                  case "LIST":
+                  case "OBJ":
+                  case "MAP":
+                  case "WAIF":
+                  case "ANON":
+                     return Settings.Instance.BuiltinVariableBackgroundColor;
+                  default:
+                     if (Moo.Builtins.ContainsKey(token.DisplayText) &&
+                         previousToken?.DisplayText != ":" &&
+                         nextToken?.DisplayText == "(")
+                        return Settings.Instance.BuiltinFunctionBackgroundColor;
+                     else
+                        return Settings.Instance.DefaultWordBackgroundColor;
+               }
+            case "'->'":
+            case "'$'":
+            case "'`'":
+            case "'''":
+            case "'=>'":
+            case "'?'":
+            case "'|'":
+            case "'@'":
+            case "';'":
+            case "':'":
+            case "'.'":
+            case "'..'":
+            case "','":
+               return Settings.Instance.SymbolBackgroundColor;
+            case "'='":
+            case "'+'":
+            case "'-'":
+            case "'*'":
+            case "'/'":
+            case "'^'":
+            case "'%'":
+            case "'<'":
+            case "'>'":
+            case "'=='":
+            case "'!='":
+            case "'!'":
+            case "'~'":
+            case "'<='":
+            case "'>='":
+            case "'&.'":
+            case "'|.'":
+            case "'^.'":
+            case "'>>'":
+            case "'<<'":
+            case "'+='":
+            case "'-='":
+            case "'*='":
+            case "'/='":
+            case "'&='":
+            case "'|='":
+            case "'%='":
+            case "'^='":
+            case "'++'":
+            case "'--'":
+            case "'||'":
+            case "'&&'":
+            case "IN":
+               return Settings.Instance.OperatorBackgroundColor;
+            case "'('":
+            case "')'":
+               return Settings.Instance.ParenthesisBackgroundColor;
+            case "'['":
+            case "']'":
+               return Settings.Instance.BracketBackgroundColor;
+            case "'{'":
+            case "'}'":
+               return Settings.Instance.CurlyBraceBackgroundColor;
+            case "SINGLE_LINE_COMMENT":
+            case "DELIMITED_COMMENT":
+               return Settings.Instance.CommentBackgroundColor;
+            default:
+               return Settings.Instance.DefaultWordBackgroundColor;
+         }
       }
 
       public FontStyle GetTokenFontStyle(DetailedToken token, DetailedToken previousToken, DetailedToken nextToken)
       {
-         return FontStyle.Regular;
+         var tokenTypeName = token.TypeNameUpperCase;
+         switch (tokenTypeName)
+         {
+            case "WHILE":
+            case "ENDWHILE":
+            case "FOR":
+            case "ENDFOR":
+            case "BREAK":
+            case "CONTINUE":
+            case "RETURN":
+            case "FORK":
+            case "ENDFORK":
+            case "TRY":
+            case "EXCEPT":
+            case "FINALLY":
+            case "ENDTRY":
+            case "IF":
+            case "ELSE":
+            case "ELSEIF":
+            case "ENDIF":
+            case "ANY":
+               return Settings.Instance.KeywordFontStyle;
+            case "OBJECT":
+               return Settings.Instance.ObjectFontStyle;
+            case "CORE_REFERENCE":
+               return Settings.Instance.CoreReferenceFontStyle;
+            case "STRING":
+               return Settings.Instance.StringFontStyle;
+            case "NUMBER":
+            case "FLOAT":
+            case "ERROR":
+            case "BOOLEAN":
+               return Settings.Instance.LiteralFontStyle;
+            case "IDENTIFIER":
+               switch (token.DisplayText)
+               {
+                  case "player":
+                  case "caller":
+                  case "this":
+                  case "verb":
+                  case "args":
+                  case "argstr":
+                  case "dobj":
+                  case "dobjstr":
+                  case "prepstr":
+                  case "iobj":
+                  case "iobjstr":
+                  case "ERR":
+                  case "STR":
+                  case "BOOL":
+                  case "INT":
+                  case "NUM":
+                  case "FLOAT":
+                  case "LIST":
+                  case "OBJ":
+                  case "MAP":
+                  case "WAIF":
+                  case "ANON":
+                     return Settings.Instance.BuiltinVariableFontStyle;
+                  default:
+                     if (Moo.Builtins.ContainsKey(token.DisplayText) &&
+                         previousToken?.DisplayText != ":" &&
+                         nextToken?.DisplayText == "(")
+                        return Settings.Instance.BuiltinFunctionFontStyle;
+                     else
+                        return Settings.Instance.DefaultWordFontStyle;
+               }
+            case "'->'":
+            case "'$'":
+            case "'`'":
+            case "'''":
+            case "'=>'":
+            case "'?'":
+            case "'|'":
+            case "'@'":
+            case "';'":
+            case "':'":
+            case "'.'":
+            case "'..'":
+            case "','":
+               return Settings.Instance.SymbolFontStyle;
+            case "'='":
+            case "'+'":
+            case "'-'":
+            case "'*'":
+            case "'/'":
+            case "'^'":
+            case "'%'":
+            case "'<'":
+            case "'>'":
+            case "'=='":
+            case "'!='":
+            case "'!'":
+            case "'~'":
+            case "'<='":
+            case "'>='":
+            case "'&.'":
+            case "'|.'":
+            case "'^.'":
+            case "'>>'":
+            case "'<<'":
+            case "'+='":
+            case "'-='":
+            case "'*='":
+            case "'/='":
+            case "'&='":
+            case "'|='":
+            case "'%='":
+            case "'^='":
+            case "'++'":
+            case "'--'":
+            case "'||'":
+            case "'&&'":
+            case "IN":
+               return Settings.Instance.OperatorFontStyle;
+            case "'('":
+            case "')'":
+               return Settings.Instance.ParenthesisFontStyle;
+            case "'['":
+            case "']'":
+               return Settings.Instance.BracketFontStyle;
+            case "'{'":
+            case "'}'":
+               return Settings.Instance.CurlyBraceFontStyle;
+            case "SINGLE_LINE_COMMENT":
+            case "DELIMITED_COMMENT":
+               return Settings.Instance.CommentFontStyle;
+            default:
+               return Settings.Instance.DefaultWordFontStyle;
+         }
       }
 
       public string GrammarName => "Moo";
