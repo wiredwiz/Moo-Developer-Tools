cfc81f603244459afa21ee2de3e8cbe789696422
Thaddeus Ryker <Thaddeus.Ryker@dansko.com>
2022-08-22 12:35:02 -0400

    Added support for tracking styles on the StyledChar without limit (other than max int).

--- FastColoredTextBox/Types/StyledChar.cs
+++ FastColoredTextBox/Types/StyledChar.cs
@@ -1,21 +1,107 @@
-﻿namespace FastColoredTextBoxNS.Types {
-	/// <summary>
-	/// Char and style
-	/// </summary>
-	public struct StyledChar {
-		/// <summary>
-		/// Unicode character
-		/// </summary>
-		public char c;
-		/// <summary>
-		/// Style bit mask
-		/// </summary>
-		/// <remarks>Bit 1 in position n means that this char will rendering by FastColoredTextBox.Styles[n]</remarks>
-		public StyleIndex style;
+﻿using System;
+using System.Linq;
+// ReSharper disable ExceptionNotDocumented
 
-		public StyledChar(char c) {
-			this.c = c;
-			style = StyleIndex.None;
-		}
-	}
+namespace FastColoredTextBoxNS.Types
+{
+    /// <summary>
+    /// Char and style
+    /// </summary>
+    public struct StyledChar
+    {
+
+        /// <summary>
+        /// Unicode character
+        /// </summary>
+        public char C;
+
+        /// <summary>
+        /// Style bit mask
+        /// </summary>
+        /// <remarks>Bit 1 in position n means that this char will rendering by FastColoredTextBox.Styles[n]</remarks>
+        public StyleIndex Style;
+
+        /// <summary>
+        /// An array of for this styled char
+        /// </summary>
+        public Style[] Styles;
+
+        /// <summary>The index of the last style assigned</summary>
+        /// <remarks>Returns -1 if there are no styles</remarks>
+        public int LastStyle;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="StyledChar"/> struct.
+        /// </summary>
+        /// <param name="c">The c.</param>
+        public StyledChar(char c)
+        {
+            this.C = c;
+            Style = StyleIndex.None;
+            Styles = null;
+            LastStyle = -1;
+        }
+
+        /// <summary>
+        /// Adds the specified style to the character.
+        /// </summary>
+        /// <param name="style">The style.</param>
+        /// <exception cref="InvalidOperationException">An attempt was made to add more than <see cref="int.MaxValue" /> style elements.</exception>
+        public void AddStyle(Style style)
+        {
+            ++LastStyle;
+            if (Styles == null)
+                Styles = new Style[2];
+            else
+            {
+                if (Styles.Contains(style))
+                    return;
+                if (LastStyle == Styles.Length)
+                    if (LastStyle == int.MaxValue)
+                        throw new InvalidOperationException($"You cannot add more than {LastStyle} styles to a character");
+                    else
+                        Array.Resize(ref Styles, Styles.Length > int.MaxValue / 2 ? int.MaxValue : Styles.Length * 2);
+
+            }
+
+            Styles[LastStyle] = style;
+        }
+
+        /// <summary>
+        /// Gets the index of the specified style.
+        /// </summary>
+        /// <param name="style">The style.</param>
+        /// <returns>The <see cref="int"/> index value.</returns>
+        public int GetStyleIndex(Style style)
+        {
+            if (Styles == null)
+                return -1;
+
+            return Array.IndexOf(Styles, style);
+        }
+
+        /// <summary>
+        /// Removes the specified style.
+        /// </summary>
+        /// <param name="style">The style.</param>
+        public void RemoveStyle(Style style)
+        {
+            var index = GetStyleIndex(style);
+            if (index == -1)
+                return;
+
+            for (int i = index; i < Styles.Length - 1; i++)
+                Styles[i] = Styles[i + 1];
+            Styles[^1] = null;
+        }
+
+        /// <summary>
+        /// Clears all styles for the character.
+        /// </summary>
+        public void ClearStyles()
+        {
+            Styles = null;
+            LastStyle = -1;
+        }
+    }
 }
