6943d666c8f833a8307a72c5ef5b3edc828c83d5
Thad Ryker <thad@edgerunner.org>
2022-08-23 22:09:36 -0400

    Added project for handling MCP.

---
+++ Org.Edgerunner.Mud.MCP/Interfaces/IMCPConfiguration.cs
@@ -0,0 +1,78 @@
+﻿#region BSD 3-Clause License
+// <copyright company="Edgerunner.org" file="IMCPConfiguration.cs">
+// Copyright (c)  2022
+// </copyright>
+//
+// BSD 3-Clause License
+//
+// Copyright (c) 2022,
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice, this
+//    list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright notice,
+//    this list of conditions and the following disclaimer in the documentation
+//    and/or other materials provided with the distribution.
+//
+// 3. Neither the name of the copyright holder nor the names of its
+//    contributors may be used to endorse or promote products derived from
+//    this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#endregion
+
+namespace Org.Edgerunner.Mud.MCP.Interfaces;
+
+/// <summary>
+/// An interface that represents a given MCP configuration
+/// </summary>
+// ReSharper disable once InconsistentNaming
+public interface IMCPConfiguration
+{
+   /// <summary>
+   /// Registers a package to be supported for new sessions.
+   /// </summary>
+   /// <param name="package">The package to register.</param>
+   /// <returns>true if the package is registered; false if it exists already</returns>
+   bool RegisterPackage(IMCPPackage package);
+
+   /// <summary>
+   /// Registers a package to be supported for new sessions.
+   /// </summary>
+   /// <param name="package">The package to register.</param>
+   /// <param name="minVersion">The minimum version.</param>
+   /// <param name="maxVersion">The maximum version.</param>
+   /// <returns>
+   /// true if the package is registered; false if it exists already
+   /// </returns>
+   bool RegisterPackage(IMCPPackage package, double minVersion, double maxVersion);
+
+   /// <summary>
+   /// Gets or sets the maximum protocol version.
+   /// </summary>
+   /// <value>
+   /// The maximum protocol version.
+   /// </value>
+   double MaximumProtocolVersion { get; set; }
+
+   /// <summary>
+   /// Gets or sets the minimum protocol version.
+   /// </summary>
+   /// <value>
+   /// The minimum protocol version.
+   /// </value>
+   double MinimumProtocolVersion { get; set; }
+}
