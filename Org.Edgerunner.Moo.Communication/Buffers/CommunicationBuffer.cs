ff657fa7217ebba26b7a6d3b502c52f12e998a9c
Thad Ryker <thad@edgerunner.org>
2022-08-25 19:55:52 -0400

    Created a communications byte buffer class.

---
+++ Org.Edgerunner.Moo.Communication/Buffers/CommunicationBuffer.cs
@@ -0,0 +1,104 @@
+﻿#region BSD 3-Clause License
+// <copyright company="Edgerunner.org" file="CommunicationBuffer.cs">
+// Copyright (c)  2022
+// </copyright>
+//
+// BSD 3-Clause License
+//
+// Copyright (c) 2022,
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice, this
+//    list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright notice,
+//    this list of conditions and the following disclaimer in the documentation
+//    and/or other materials provided with the distribution.
+//
+// 3. Neither the name of the copyright holder nor the names of its
+//    contributors may be used to endorse or promote products derived from
+//    this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#endregion
+
+using System.Text;
+using Org.Edgerunner.Common.Buffers;
+
+namespace Org.Edgerunner.Moo.Communication.Buffers;
+
+/// <summary>
+/// A class that represents a moo communication byte buffer
+/// </summary>
+/// <seealso cref="Org.Edgerunner.Common.Buffers.ConcurrentCircularBuffer&lt;System.Byte&gt;" />
+public class CommunicationBuffer : ConcurrentCircularBuffer<byte>
+{
+   /// <summary>
+   /// Initializes a new instance of the <see cref="CommunicationBuffer"/> class.
+   /// </summary>
+   /// <param name="capacity">Buffer capacity. Must be positive.</param>
+   public CommunicationBuffer(int capacity)
+      : base(capacity)
+   {
+   }
+
+   /// <summary>
+   /// Initializes a new instance of the <see cref="CommunicationBuffer"/> class.
+   /// </summary>
+   /// <param name="capacity">Buffer capacity. Must be positive.</param>
+   /// <param name="items">Items to fill buffer with. Items length must be less than capacity.
+   /// Suggestion: use Skip(x).Take(y).ToArray() to build this argument from
+   /// any enumerable.</param>
+   public CommunicationBuffer(int capacity, byte[] items)
+      : base(capacity, items)
+   {
+   }
+
+   public virtual string? PopTextLine(Decoder decoder)
+   {
+      byte[] buffer;
+      int position;
+
+      lock (SyncLock)
+      {
+         if (IsEmpty)
+            return null;
+
+         var endOfLine = -1;
+         for (int i = 0; i < Size; i++)
+         {
+            if (this[i] == '\n')
+            {
+               endOfLine = i;
+               break;
+            }
+         }
+
+         if (endOfLine == -1)
+            return null;
+
+         buffer = new byte[endOfLine + 1];
+         position = 0;
+
+         while (!IsEmpty)
+         {
+            buffer[position] = PopFront();
+            position++;
+         }
+      }
+
+      return new string(new char[decoder.GetCharCount(buffer, 0, position)]);
+   }
+}
