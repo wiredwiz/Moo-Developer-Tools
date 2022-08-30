4d4110dc41169a14bf2515577f5b010ea4228d65
Thaddeus Ryker <Thaddeus.Ryker@dansko.com>
2022-08-30 15:42:29 -0400

    Modified auto-indent logic to solely rely on the parser.

--- Org.Edgerunner.Moo.Editor/Controls/MooEditor.cs
+++ Org.Edgerunner.Moo.Editor/Controls/MooEditor.cs
@@ -500,65 +500,12 @@ namespace Org.Edgerunner.Moo.Editor.Controls
 
       private void MooEditor_AutoIndentNeeded(object? sender, AutoIndentEventArgs e)
       {
-         //if (IndentationGuide != null)
-         //{
-         //   var indentShift = IndentationGuide.GetIndentShift(e.ILine + 1);
-         //   e.Shift = indentShift;
-         //   e.ShiftNextLines = indentShift;
-         //   return;
-         //}
-
-         bool indent = Regex.IsMatch(e.PrevLineText, @"\b(if|while|fork|for|try|elseif|else|except|finally)\b");
-         bool previousTerminates = Regex.IsMatch(e.PrevLineText, @"\b(endif|endfork|endfor|endwhile|endtry)\b");
-         bool currentIndents = Regex.IsMatch(e.LineText, @"\b(if|fork|for|while|try)\b");
-         bool unIndent = Regex.IsMatch(e.LineText,
-            @"\b(elseif|else|except|finally|endif|endfork|endfor|endwhile|endtry)\b");
-         bool isCommenting = Regex.IsMatch(e.PrevLineText, @".*/\*.*");
-         bool terminatesComment = Regex.IsMatch(e.PrevLineText, @".*\*/.*");
-
-         // Check for multi-line comments
-         if (isCommenting)
+         if (IndentationGuide != null)
          {
-            e.Shift = 3;
-            e.ShiftNextLines = 3;
+            var indentShift = IndentationGuide.GetIndentShift(e.ILine + 1);
+            e.Shift = indentShift;
+            e.ShiftNextLines = indentShift;
             return;
-         }
-         if (terminatesComment)
-         {
-            e.Shift = -3;
-            e.ShiftNextLines = -3;
-            return;
-         }
-
-         // The previous line had something like "if (foo) bah; endif" and the current line contains something like "else"
-         if (indent && previousTerminates && unIndent)
-         {
-            e.Shift = -e.TabLength;
-            e.ShiftNextLines = -e.TabLength;
-            return;
-         }
-
-         // The previous line had something like "if (foo) bah; endif" and our current line has nothing special
-         if (indent && previousTerminates)
-            return;
-
-         // The previous line starting an indentation block and our current line does not contain something that would cause an un-indent
-         if (indent && !unIndent)
-         {
-            e.Shift = e.TabLength;
-            e.ShiftNextLines = e.TabLength;
-            return;
-         }
-
-         // Our current line contains something like "if (foo) bah; endif"
-         if (unIndent && currentIndents)
-            return;
-
-         // Lastly our current line contains something that would cause an un-indent and the previous line does not cause an indent
-         if (unIndent && !indent)
-         {
-            e.Shift = -e.TabLength;
-            e.ShiftNextLines = -e.TabLength;
          }
       }
    }
