5a4547073e416835f2f92430b76fa7a9c13a5485
Thad Ryker <thad@edgerunner.org>
2022-09-08 21:20:22 -0400

    Cleaning up the About box form code.

--- Org.Edgerunner.Moo.Udditor/AboutBox.cs
+++ Org.Edgerunner.Moo.Udditor/AboutBox.cs
@@ -12,108 +12,108 @@ using Krypton.Toolkit;
 namespace Org.Edgerunner.Moo.Udditor;
 partial class AboutBox : KryptonForm
 {
-    public AboutBox()
-    {
-        InitializeComponent();
-        this.Text = String.Format("About {0}", AssemblyTitle);
-        this.labelProductName.Text = AssemblyProduct;
-        this.lblProject.Text = ProjectUrl;
-        this.labelVersion.Text = String.Format("Version {0}", AssemblyVersion);
-        this.labelCopyright.Text = AssemblyCopyright;
-        this.labelCompanyName.Text = AssemblyCompany;
-        this.textBoxDescription.Text = AssemblyDescription;
-    }
+   public AboutBox()
+   {
+      InitializeComponent();
+      this.Text = String.Format("About {0}", AssemblyTitle);
+      this.labelProductName.Text = AssemblyProduct;
+      this.lblProject.Text = ProjectUrl;
+      this.labelVersion.Text = String.Format("Version {0}", AssemblyVersion);
+      this.labelCopyright.Text = AssemblyCopyright;
+      this.labelCompanyName.Text = AssemblyCompany;
+      this.textBoxDescription.Text = AssemblyDescription;
+   }
 
-    public static AboutBox Instance { get; set; } = new AboutBox();
+   public static AboutBox Instance { get; set; } = new AboutBox();
 
-    #region Assembly Attribute Accessors
+   #region Assembly Attribute Accessors
 
-    public string AssemblyTitle
-    {
-        get
-        {
-            object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyTitleAttribute), false);
-            if (attributes.Length > 0)
+   public string AssemblyTitle
+   {
+      get
+      {
+         object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyTitleAttribute), false);
+         if (attributes.Length > 0)
+         {
+            AssemblyTitleAttribute titleAttribute = (AssemblyTitleAttribute)attributes[0];
+            if (titleAttribute.Title != "")
             {
-                AssemblyTitleAttribute titleAttribute = (AssemblyTitleAttribute)attributes[0];
-                if (titleAttribute.Title != "")
-                {
-                    return titleAttribute.Title;
-                }
+               return titleAttribute.Title;
             }
-            return System.IO.Path.GetFileNameWithoutExtension(Assembly.GetExecutingAssembly().CodeBase);
-        }
-    }
+         }
+         return System.IO.Path.GetFileNameWithoutExtension(Assembly.GetExecutingAssembly().CodeBase);
+      }
+   }
 
-    public string AssemblyVersion
-    {
-        get
-        {
-            return Assembly.GetExecutingAssembly().GetName().Version.ToString();
-        }
-    }
+   public string AssemblyVersion
+   {
+      get
+      {
+         return Assembly.GetExecutingAssembly().GetName().Version?.ToString() ?? new Version(0,0).ToString();
+      }
+   }
 
    public string ProjectUrl => "https://github.com/wiredwiz/Moo-Developer-Tools";
 
    public string AssemblyDescription
-    {
-        get
-        {
-            object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyDescriptionAttribute), false);
-            if (attributes.Length == 0)
-            {
-                return "";
-            }
-            return ((AssemblyDescriptionAttribute)attributes[0]).Description;
-        }
-    }
+   {
+      get
+      {
+         object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyDescriptionAttribute), false);
+         if (attributes.Length == 0)
+         {
+            return "";
+         }
+         return ((AssemblyDescriptionAttribute)attributes[0]).Description;
+      }
+   }
 
-    public string AssemblyProduct
-    {
-        get
-        {
-            object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyProductAttribute), false);
-            if (attributes.Length == 0)
-            {
-                return "";
-            }
-            return ((AssemblyProductAttribute)attributes[0]).Product;
-        }
-    }
+   public string AssemblyProduct
+   {
+      get
+      {
+         object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyProductAttribute), false);
+         if (attributes.Length == 0)
+         {
+            return "";
+         }
+         return ((AssemblyProductAttribute)attributes[0]).Product;
+      }
+   }
 
-    public string AssemblyCopyright
-    {
-        get
-        {
-            object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyCopyrightAttribute), false);
-            if (attributes.Length == 0)
-            {
-                return "";
-            }
-            return ((AssemblyCopyrightAttribute)attributes[0]).Copyright;
-        }
-    }
+   public string AssemblyCopyright
+   {
+      get
+      {
+         object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyCopyrightAttribute), false);
+         if (attributes.Length == 0)
+         {
+            return "";
+         }
+         return ((AssemblyCopyrightAttribute)attributes[0]).Copyright;
+      }
+   }
 
-    public string AssemblyCompany
-    {
-        get
-        {
-            object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyCompanyAttribute), false);
-            if (attributes.Length == 0)
-            {
-                return "";
-            }
-            return ((AssemblyCompanyAttribute)attributes[0]).Company;
-        }
-    }
+   public string AssemblyCompany
+   {
+      get
+      {
+         object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyCompanyAttribute), false);
+         if (attributes.Length == 0)
+         {
+            return "";
+         }
+         return ((AssemblyCompanyAttribute)attributes[0]).Company;
+      }
+   }
    #endregion
 
    private void lnkProject_LinkClicked(object sender, EventArgs e)
    {
-      System.Diagnostics.Process.Start(new ProcessStartInfo
-                                       {
-                                          FileName = ProjectUrl,
-                                          UseShellExecute = true
-                                       });
+      Process.Start(new ProcessStartInfo
+      {
+         FileName = ProjectUrl,
+         UseShellExecute = true
+      });
    }
 }
